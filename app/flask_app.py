f53f6931198588487f2efcfa34e342ccad0eb414
Corrin Lakeland <corrin.lakeland@countdown.co.nz>
2024-06-08 16:28:09 +1200

    Added .idea to ignore

--- app/flask_app.py
+++ app/flask_app.py
@@ -1,11 +1,16 @@
 from flask import Flask
 from flask import Blueprint, render_template
+from flask_login import LoginManager
+
+login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
 
     # Configure the app
     app.config.from_object('config.Config')
+
+    login_manager.init_app(app)
 
     # Register blueprints
     from .routes import main_bp
